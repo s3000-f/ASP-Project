ea26b71eb1a3c45bc8c7b71a2a2b42fbd13abcd4
s3000 <s3000@yahoo.com>
2019-01-27 16:11:01 +0330

    FIxed User Model

--- ASP-Backend/Models/User.cs
+++ ASP-Backend/Models/User.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace ASP_Backend.Models
 {
     public class User
     {
-        public User()
-        {
-        }
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public virtual IList<Post> Posts { get; set; }
     }
 }
