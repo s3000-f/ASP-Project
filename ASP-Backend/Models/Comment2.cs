e3ceb8e6336863d6fe53eb817cfb57b0e751bf31
Kimia <kimia_kimi77@yahoo.com>
2019-02-03 00:08:52 +0330

    new models created.

---
+++ ASP-Backend/Models/Comment2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASP_Backend.Models
+{
+    public class Comment2
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int? UserId { get; set; }
+        public int? PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
---
+++ ASP-Backend/Models/Post2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASP_Backend.Models
+{
+    public class Post2
+    {
+        public int Id { get; set; }
+        public Guid Name { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Caption { get; set; }
+    }
+}
